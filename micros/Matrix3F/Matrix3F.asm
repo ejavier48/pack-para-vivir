3d2ba5de602ec28550508f27653b3813b3cdaf61
ejavier48 <esanchezg1104@alumno.ipn.mx>
2019-11-23 13:41:22 -0600

    asm code for matrix, and matrix moving 3 figures and restart

--- micros/Matrix3F/Matrix3F.asm
+++ micros/Matrix3F/Matrix3F.asm
@@ -7,6 +7,8 @@
 .def col=r17
 .def aux=r18
 .def cont=r19
+.def offset=r20
+.def times=r21
 
 rjmp main
 .org $009
@@ -53,12 +55,34 @@ change:
     ldi col, $01
     ldi ZL, low(mat<<1)
     ldi ZH, high(mat<<1)
+	inc times
+	cpi times, 50
+	brsh move
     ldi row, $00
     out porta, row
+	add ZL, offset
+    lpm row, z+
+    reti
+move: ;move the figure one val at a time 
+	inc offset
+	cpi offset, 17
+	brsh again
+	clr times
+    ldi row, $00
+    out porta, row
+	add ZL, offset
+    lpm row, z+
+    reti
+again: ; Restart from first figure
+	clr times
+	clr offset
+    ldi row, $00
+    out porta, row
+	add ZL, offset
     lpm row, z+
     reti
 return:
 	reti
 mat:
-	.db $FF, $7E, $3C, $18, $3C, $7E, $FF, $00
-;cols      0,   1,   2,   3,   4,   5,   6,   7
+	.db $18, $24, $42, $81, $81, $42, $24, $18, $18, $3C, $66, $C3, $C3,  $66, $3C, $18, $18, $3C, $7E, $E7, $E7, $7E , $3C, $18
+	;cols      0,   1,   2,   3,   4,   5,   6,   7   
